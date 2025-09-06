89e6fd666f114862f4aa03d642b4db0728c02a83
Luke Ponga <developmentdesignsltd@gmail.com>
2025-09-06 23:37:57 +0000

    Make font modern

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
+        body: ['"Inter"', 'sans-serif'],
         headline: ['"Space Grotesk"', 'sans-serif'],
         code: ['monospace'],
       },
